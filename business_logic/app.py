b71eee72773faf7b638b429b1715b8829dbef49a
unknown <PerlaSaikaly2002@outlook.comgit config --global user.name Perla-Saikaly>
2024-04-30 20:36:44 +0300

    modify flask

--- business_logic/app.py
+++ business_logic/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route('/services', methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
